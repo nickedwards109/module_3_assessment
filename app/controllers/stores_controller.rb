3b44838933eb65da56945e75fa235b1245c5912d
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-07-26 11:00:56 -0600

    progress on getting stores from best buy api

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,7 +3,9 @@ class StoresController < ApplicationController
   end
 
   def index
-    # params[:location]
+    connection = Faraday.new(url: "https://api.bestbuy.com/v1/stores(postalCode=#{params[:postal_code]})?format=json&show=longName,city,distance,phone,storeType&apiKey=#{ENV['BEST_BUY_API_KEY']}")
+    response = connection.get
+    raw_stores = JSON.parse(response.body, symbolize_names: true)
     binding.pry
   end
 end
