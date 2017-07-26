08551989a6c5e3eecaf1461103e29971f564366b
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-07-26 09:46:48 -0600

    specify GET /api/v1/items/:id

--- spec/requests/api/v1/user_can_get_items_spec.rb
+++ spec/requests/api/v1/user_can_get_items_spec.rb
@@ -21,4 +21,22 @@ describe 'Getting items' do
     expect(item).not_to have_key(:created_at)
     expect(item).not_to have_key(:updated_at)
   end
+
+  it 'returns a status 200 JSON response containing a single item' do
+    item = Item.create(name: "Item 1 Name", description: "Item 1 Description", image_url: "image1.jpg")
+    get "/api/v1/items/#{item.id}"
+    expect(response).to have_http_status(:ok)
+
+    item = JSON.parse(response.body, symbolize_names: true)
+    expect(item).to be_a Hash
+    expect(item).to have_key(:id)
+    expect(item).to have_key(:name)
+    expect(item[:name]).to eq("Item 1 Name")
+    expect(item).to have_key(:description)
+    expect(item[:description]).to eq("Item 1 Description")
+    expect(item).to have_key(:image_url)
+    expect(item[:image_url]).to eq("image1.jpg")
+    expect(item).not_to have_key(:created_at)
+    expect(item).not_to have_key(:updated_at)
+  end
 end
