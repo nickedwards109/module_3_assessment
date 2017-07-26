d489d299461305315381329b2cea3e6753efd928
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-07-26 11:33:26 -0600

    modify request to get all stores instead of the default limit

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -3,10 +3,9 @@ class StoresController < ApplicationController
   end
 
   def index
-    connection = Faraday.new(url: "https://api.bestbuy.com/v1/stores(area(#{params[:postal_code]},25))?format=json&show=longName,city,distance,phone,storeType&apiKey=#{ENV['BEST_BUY_API_KEY']}")
+    connection = Faraday.new(url: "https://api.bestbuy.com/v1/stores(area(#{params[:postal_code]},25))?format=json&show=longName,city,distance,phone,storeType&pageSize=100&apiKey=#{ENV['BEST_BUY_API_KEY']}")
     response = connection.get
     raw_stores = JSON.parse(response.body, symbolize_names: true)
-    @stores_count = raw_stores[:total]
     @stores = raw_stores[:stores]
   end
 end
