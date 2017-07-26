9bebd08ec6e1ab6520fd5ad691a21f339ebc8d86
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-07-26 12:12:39 -0600

    fix incorrect stores count display

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,10 +1,13 @@
+require 'will_paginate/array'
+
 class StoresController < ApplicationController
   def search
   end
 
   def index
     search_location = params[:postal_code]
-    @stores = Store.all_near(search_location)
-    @stores = @stores.paginate(page: params[:page], per_page: 10)
+    stores = Store.all_near(search_location)
+    @stores_count = stores.count
+    @stores = stores.paginate(page: params[:page], per_page: 10)
   end
 end
