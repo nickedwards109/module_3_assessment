e7732b33b5b8eda4d3d3301d8df58f04deb36ab6
nicholasedwards <nicholas.lee.edwards@gmail.com>
2017-07-26 10:20:41 -0600

    pass spec for POST /api/v1/items
    I have not specified or implemented a sad path for item creation since this is not explicitly specified in the project requirements.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,10 +9,22 @@ class Api::V1::ItemsController < ApplicationController
     render json: Item.find(params[:id])
   end
 
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item, status: :created
+    end
+  end
+
   def destroy
     item = Item.find(params[:id])
     if item.destroy
       render json: {}, status: :no_content
     end
   end
+
+  private
+    def item_params
+      params.require(:item).permit(:name, :description, :image_url)
+    end
 end
